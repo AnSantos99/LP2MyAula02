6466002b33de552f42e096187d44ce4ad528d098
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-10-09 17:23:36 +0100

    Using 4th version of GetStuff

--- Tuples/Program.cs
+++ Tuples/Program.cs
@@ -6,10 +6,7 @@ namespace Tuples
     {
         private static Random rnd;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
+        /*
         private static object[] GetStuff1() 
         {
             // Create array of type objects with 3 "houses"
@@ -19,17 +16,44 @@ namespace Tuples
             objs[2] = rnd.NextDouble() < 0.5;
 
             return objs;
+        }*/
+
+        /*
+    private static Stuff GetStuff() 
+    {
+        return new Stuff(rnd.Next(), rnd.NextDouble(), rnd.NextDouble() < 0.5);
+    }*/
+        /*
+        /// <summary>
+        /// Method to return random numbers with keyword out
+        /// </summary>
+        /// <param name="a"> Receive int</param>
+        /// <param name="b"> Receive double</param>
+        /// <param name="c"> Receive bool</param>
+        private static void GetStuff(out int a, out double b, out bool c) 
+        {
+            a = rnd.Next();
+            b = rnd.NextDouble();
+            c = rnd.NextDouble() < 0.5;
+        }*/
+
+        private static Tuple<int, double, bool> GetStuff() 
+        {
+            Tuple<int, double, bool> stuff = new Tuple<int, double, bool>
+                (rnd.Next(), rnd.NextDouble(), rnd.NextDouble() < 0.5);
+            return stuff;
         }
+
 
         static void Main(string[] args)
         {
             rnd = new Random();
 
-            object[] objs = GetStuff1();
-            foreach (object o in objs) 
-            {
-                Console.WriteLine(o);
-            }
+            Tuple<int, double, bool> t = GetStuff();
+
+            Console.WriteLine("In= "+ t.Item1 + " do= "+ t.Item2 + " Bo= "+ t.Item3);
+
+            Console.WriteLine();
         }
     }
 }
--- Tuples/Stuff.cs
+++ Tuples/Stuff.cs
@@ -6,6 +6,18 @@ namespace Tuples
 {
     struct Stuff
     {
+        public int IntenStuff { get; }
+        public double DoubStuff { get; }
+        public bool BooStuff { get; }
+
+        public Stuff(int a, double b, bool c) 
+        {
+            IntenStuff = a;
+            DoubStuff = b;
+            BooStuff = c;
+
+        }
+
 
     }
 }
